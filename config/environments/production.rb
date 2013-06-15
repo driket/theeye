932fdb1bbc84fac9b22f3b8699ba5fb9d12764dc
Cedric Villa <cedric.villa@gmail.com>
2013-06-15 13:14:53 +0200

    add basic auth test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Dashboard::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Production") do |u, p|
+    [u, p] == ['username', 'password']
+  end
 end
