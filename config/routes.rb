ed5e8d90d1e54e4ead34040d1fe6c02658b0d4f7
Cedric Villa <cedric.villa@gmail.com>
2013-06-16 16:25:05 +0200

    routes -> default to widgets#index

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Dashboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'widgets#index'
 
   # See how all your routes lay out with "rake routes"
 
