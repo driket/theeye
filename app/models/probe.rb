504552c25cd090f471e6ae361e1aaf3e572d3c5e
Cedric Villa <cedric.villa@gmail.com>
2013-07-24 09:11:04 +0200

    add fetch_default_values (weird way of doing this, I was drunk last night)

--- app/models/probe.rb
+++ app/models/probe.rb
@@ -3,6 +3,16 @@ class Probe < ActiveRecord::Base
   has_many :widgets
   accepts_nested_attributes_for :widgets, :allow_destroy => true
   
+  before_validation :fetch_probe_default_data
+  
+  def fetch_probe_default_data 
+    logger.debug "---------> record = #{self}"
+    # list all widgets and set their default values
+    for widget in widgets
+      widget_command_data = widget.fetch_default_values
+    end
+  end
+  
   require 'open-uri'
   require 'json'
   
