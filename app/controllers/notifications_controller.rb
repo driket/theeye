0ad3c433440f50a959382cc414077de447d4f90e
Cedric Villa <cedric.villa@gmail.com>
2013-08-02 17:28:17 +0200

    show only 50 most recent notifications, inverse order

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   # GET /notifications
   # GET /notifications.json
   def index
-    @notifications = Notification.all
+    @notifications = Notification.last(50).reverse
 
     respond_to do |format|
       format.html # index.html.erb
