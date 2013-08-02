4c1db660e60a82458815f958c96bfd3cc2707412
Cedric Villa <cedric.villa@gmail.com>
2013-08-02 15:26:21 +0200

    add notification skeleleton method for samples

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,4 +1,14 @@
 class Sample < ActiveRecord::Base
   
   belongs_to :widget
+  after_save :check_for_notifications
+  
+  def check_for_notifications
+  	# send alert notification if status has changed to'alert' since 3 samples
+  	# don't send notification if already sent
+
+  	# send recovery notification if status has changed to 'ok' since 3 samples
+  	# don't send notification if already sent
+    logger.debug "check_for_notifications"
+  end
 end
