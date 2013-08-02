0d543f97d321ef9db17634cd72eb1138ccfc92b1
Cedric Villa <cedric.villa@gmail.com>
2013-08-02 17:14:17 +0200

    update with notifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130802120642) do
+ActiveRecord::Schema.define(:version => 20130802140454) do
+
+  create_table "notifications", :force => true do |t|
+    t.integer  "widget_id"
+    t.datetime "date"
+    t.integer  "status"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "probes", :force => true do |t|
     t.string   "title"
