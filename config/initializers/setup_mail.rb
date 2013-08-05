5da2cb7276b6ff9f12dcfb3f3693ba9fc7e8674c
Cedric Villa <cedric.villa@gmail.com>
2013-08-05 17:32:03 +0200

    add use_tls option for mailer in settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,5 +5,5 @@ ActionMailer::Base.smtp_settings = {
   :user_name            => Settings.mailer.user_name,  
   :password             => Settings.mailer.password,  
   :authentication       => Settings.mailer.authentication,
-  :enable_starttls_auto => true,
+  :enable_starttls_auto => Settings.mailer.enable_starttls_auto,
 }  
