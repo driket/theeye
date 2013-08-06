2c3f789e06e1247eee4549cff5334bd53be31d1b
Cedric Villa <cedric.villa@gmail.com>
2013-08-06 14:21:41 +0200

    code cleanup

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,15 +11,4 @@ every 2.minutes do
  runner "Widget.record_all_samples!"
 end
 
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
