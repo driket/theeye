6f9533394477d16a5bea8b8e9dbf2ab9b9542478
Cedric Villa <cedric.villa@gmail.com>
2013-08-30 15:23:41 +0200

    health score -> assume warning state is ok

--- app/models/probe.rb
+++ app/models/probe.rb
@@ -3,10 +3,17 @@ class Probe < ActiveRecord::Base
   has_many :widgets
   accepts_nested_attributes_for :widgets, :allow_destroy => true
   
+  STATUS = {
+    'ok'            => 0,
+    'alert'         => 1,
+    'warning'       => 2,
+    'unresponsive'  => 3,
+  }
+  
   def health
     total_health = 0
     for widget in widgets
-      total_health += 1 if widget.status == 0
+      total_health += 1 if widget.status == STATUS['ok'] || widget.status == STATUS['warning']
     end
     (widgets.size>0)?(total_health / widgets.size * 100):100
   end
